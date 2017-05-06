3e73b18e4f36671908d7c58c8198623dc7f9fc03
azeezolaniran2016 <azeezolaniran2016@gmail.com>
2017-05-06 19:49:43 +0100

    Setup Webpack loaders configuration

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; // import React package
 import ReactDOM from 'react-dom'; // import ReactDOM package
+import './styles/main.css'; // import our external css file
 
 // using the render method, we will mount this node into our DOM (html file) on the element with 
 // id of 'app'
--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,25 @@ const config = {
     filename: 'bundle.js' // output bundled file name
   },
   module: { // define our loaders here
-    rules: [ // array of rules to handle different file types
+    // array of rules to handle different file types
+    rules: [
       {
         test: /\.(js|jsx)$/, // check for .js and .jsx files (uses Regex)
-        loader: 'babel-loader' // use these loaders for .js and .jsx files found
+        loader: 'babel-loader' // use this loader for .js and .jsx files found
+      },
+      { 
+        // check for files ending with  .css (uses Regex)
+        test: /\.css$/,
+         // use these loaders of .css files. 'css-loader gets run first and is 
+         // used to handle the imports of our css files inside our jsx files. 
+         // The style loader then mounts our css in to the DOM
+        loaders: ['style-loader', 'css-loader']
       }
     ]
+  },
+  // set the file extensions we want webpack to resolve
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
 
